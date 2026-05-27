ba9f7fa1d3dbed63880a478308ec82728c54ce82
V-Karch <luna.karch05@gmail.com>
2026-05-27 15:12:54 -0400

    [Day 1 | Part 2] Completed

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,30 +1,24 @@
-#[derive(Debug)]
-enum Direction {
-    Left,
-    Right
-}
-
 pub fn part_one(lines: &Vec<String>) {
     let mut base = 50;
     let mut times_resting_at_zero = 0;
 
     for line in lines {
         let (raw_direction, number) = line.split_at(1);
-        let raw_direction = raw_direction.chars().next().unwrap();
+        let direction = raw_direction.chars().next().unwrap();
 
-        let direction: Direction = if raw_direction == 'R' {Direction::Right} else {Direction::Left};
         let number: i32 = number.parse().unwrap();
 
         match direction {
-            Direction::Left => {
+            'L' => {
                 base = (base - number) % 100;
                 if base < 0 {
                     base += 100;
                 }
-            },
-            Direction::Right => {
+            }
+            'R' => {
                 base = (base + number) % 100;
-            },
+            }
+            _ => {}
         }
 
         if base == 0 {
@@ -33,4 +27,47 @@ pub fn part_one(lines: &Vec<String>) {
     }
 
     println!("Part 1: {}", times_resting_at_zero);
+}
+
+pub fn part_two(lines: &Vec<String>) {
+    let mut base: i32 = 50;
+    let mut boundary_crossings: i32 = 0;
+
+    for line in lines {
+        let (direction_char, distance_str) = line.split_at(1);
+        let direction_char = direction_char.chars().next().unwrap();
+        let distance: i32 = distance_str.parse().unwrap();
+
+        match direction_char {
+            'R' => {
+                let distance_next = (100 - base).rem_euclid(100);
+
+                if distance_next == 0 {
+                    boundary_crossings += distance / 100;
+                } else if distance_next <= distance {
+                    boundary_crossings += 1 + (distance - distance_next) / 100;
+                }
+
+                base = (base + distance).rem_euclid(100);
+            }
+
+            'L' => {
+                let mut distance_back = base.rem_euclid(100);
+
+                if distance_back == 0 {
+                    distance_back = 100;
+                }
+
+                if distance_back <= distance {
+                    boundary_crossings += 1 + (distance - distance_back) / 100;
+                }
+
+                base = (base - distance).rem_euclid(100);
+            }
+
+            _ => {}
+        }
+    }
+
+    println!("Part 2: {}", boundary_crossings);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::fs;
 mod day_1;
 
 fn file_to_lines(path: &str) -> Vec<String> {
-    let content: String = fs::read_to_string(path)
-        .expect("Failed to read file");
+    let content: String = fs::read_to_string(path).expect("Failed to read file");
 
     return content.lines().map(String::from).collect();
 }
 
 fn main() {
-    let lines: Vec<String> = file_to_lines("inputs/day_1/input.txt");
-    day_1::part_one(&lines);
+    let day_1_lines: Vec<String> = file_to_lines("inputs/day_1/input.txt");
+    day_1::part_one(&day_1_lines);
+    day_1::part_two(&day_1_lines);
 }
